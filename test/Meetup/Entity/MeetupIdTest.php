a65b4fc9246d975261bcefb447c342b2291673d7
Matthias Noback <matthiasnoback@gmail.com>
2017-06-19 15:10:24 +0200

    Upgrade tools

--- test/Meetup/Entity/MeetupIdTest.php
+++ test/Meetup/Entity/MeetupIdTest.php
@@ -12,7 +12,7 @@ final class MeetupIdTest extends \PHPUnit_Framework_TestCase
      */
     public function it_can_be_constructed_from_a_string_and_reverted_back_to_it()
     {
-        $id = 'the ID';
+        $id = '7d7fd0b2-0cb5-42ac-b697-3f7bfce24df9';
         $meetupId = MeetupId::fromString($id);
         $this->assertSame($id, (string)$meetupId);
     }
@@ -22,8 +22,8 @@ final class MeetupIdTest extends \PHPUnit_Framework_TestCase
      */
     public function it_can_be_compared_to_another_meetup_id()
     {
-        $meetupId1 = MeetupId::fromString('id');
-        $meetupId2 = MeetupId::fromString('id');
+        $meetupId1 = MeetupId::fromString('3a021c08-ad15-43aa-aba3-8626fecd39a7');
+        $meetupId2 = MeetupId::fromString('3a021c08-ad15-43aa-aba3-8626fecd39a7');
         $this->assertTrue($meetupId1->equals($meetupId2));
     }
 }
