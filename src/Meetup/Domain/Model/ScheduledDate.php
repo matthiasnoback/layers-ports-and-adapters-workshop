485ae5609ee7143d0eb90f31e9ad6691b8fab95e
Matthias Noback <matthiasnoback@gmail.com>
2018-05-02 11:43:46 +0200

    Make ScheduledDate work with deptrac

--- src/Meetup/Domain/Model/ScheduledDate.php
+++ src/Meetup/Domain/Model/ScheduledDate.php
@@ -5,7 +5,7 @@ namespace Meetup\Domain\Model;
 
 final class ScheduledDate
 {
-    private const DATE_TIME_FORMAT = \DateTime::ATOM;
+    const DATE_TIME_FORMAT = \DateTime::ATOM;
 
     /**
      * @var string
