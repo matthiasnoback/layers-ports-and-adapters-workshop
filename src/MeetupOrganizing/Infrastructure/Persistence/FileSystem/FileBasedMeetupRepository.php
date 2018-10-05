442e30fcea88457835cdc57edc16eef1e8f14048
Matthias Noback <matthiasnoback@gmail.com>
2018-10-05 16:39:43 +0200

    Fix FileBasedMeetupRepository

--- src/MeetupOrganizing/Infrastructure/Persistence/FileSystem/FileBasedMeetupRepository.php
+++ src/MeetupOrganizing/Infrastructure/Persistence/FileSystem/FileBasedMeetupRepository.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace MeetupOrganizing\Infrastructure\Persistence\FileSystem;
 
 use MeetupOrganizing\Domain\Model\Meetup;
-use MeetupOrganizing\Domain\Model\MeetupId;
 use MeetupOrganizing\Infrastructure\Persistence\Common\AbstractMeetupRepository;
 use NaiveSerializer\Serializer;
 
@@ -18,38 +17,6 @@ final class FileBasedMeetupRepository extends AbstractMeetupRepository
     public function __construct($filePath)
     {
         $this->filePath = $filePath;
-    }
-
-    public function add(Meetup $meetup): void
-    {
-        $meetups = $this->allMeetups();
-        $meetups[] = $meetup;
-        file_put_contents($this->filePath, Serializer::serialize($meetups));
-    }
-
-    public function byId(MeetupId $meetupId): Meetup
-    {
-        foreach ($this->allMeetups() as $meetup) {
-            if ($meetup->id() === (string)$meetupId) {
-                return $meetup;
-            }
-        }
-
-        throw new \RuntimeException('Meetup not found');
-    }
-
-    public function upcomingMeetups(\DateTimeImmutable $now): array
-    {
-        return array_values(array_filter($this->allMeetups(), function (Meetup $meetup) use ($now) {
-            return $meetup->isUpcoming($now);
-        }));
-    }
-
-    public function pastMeetups(\DateTimeImmutable $now): array
-    {
-        return array_values(array_filter($this->allMeetups(), function (Meetup $meetup) use ($now) {
-            return !$meetup->isUpcoming($now);
-        }));
     }
 
     public function allMeetups(): array
