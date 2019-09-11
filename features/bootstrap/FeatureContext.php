b15d55b1cf758cdea98601753c99bd7da3b99e69
Matthias Noback <matthiasnoback@gmail.com>
2019-09-11 16:18:44 +0200

    Add a test for RSVP and attendees list

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,16 +1,21 @@
 <?php
 
-use Behat\Behat\Context\Context;
-use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
 use MeetupOrganizing\Entity\MeetupRepository;
 
 /**
  * Defines application features from the specific context.
  */
-final class FeatureContext implements Context, SnippetAcceptingContext
+final class FeatureContext extends MinkContext
 {
+    /**
+     * @var int|null
+     */
+    private $scheduledMeetupId;
+
     /**
      * Initializes context.
      *
@@ -32,5 +37,53 @@ final class FeatureContext implements Context, SnippetAcceptingContext
         /** @var MeetupRepository $meetupRepository */
         $meetupRepository = $container[MeetupRepository::class];
         $meetupRepository->deleteAll();
+    }
+
+    /**
+     * @Given /^I am logged in as "([^"]*)"$/
+     */
+    public function iAmLoggedInAs(string $name): void
+    {
+        $this->visit('/');
+        $this->selectOption('Logged in user', 'Regular user');
+        $this->pressButton('Switch');
+    }
+
+    /**
+     * @Given a meetup was scheduled
+     */
+    public function aMeetupWasScheduled(): void
+    {
+        $this->visit('/schedule-meetup');
+        $this->fillField('Name', 'Meetup');
+        $this->fillField('Description', 'Description');
+        $this->fillField('Schedule for date', '2020-10-10');
+        $this->fillField('Time', '20:00');
+        $this->pressButton('Schedule this meetup');
+
+        $currentUrl = $this->getSession()->getCurrentUrl();
+        $matches = [];
+        if (preg_match('#^.+/(\d+)$#', $currentUrl, $matches) === 0) {
+            throw new RuntimeException('Cannot determine the ID of the meetup');
+        }
+
+        $this->scheduledMeetupId = (int)$matches[1];
+    }
+
+    /**
+     * @Given I am on the detail page of this meetup
+     */
+    public function iAmOnLookingAtTheDetailPageOfThisMeetup(): void
+    {
+        $this->visit('/meetup/' . $this->scheduledMeetupId);
+    }
+
+    /**
+     * @Then the list of attendees should contain :name
+     */
+    public function theListOfAttendeesShouldContain(string $name)
+    {
+        $attendeeElement = $this->getSession()->getPage()->find('css', '.attendees li:contains("' . $name . '")');
+        assertNotNull($attendeeElement, 'Could not find the expected attendee element on the page');
     }
 }
--- features/schedule_a_meetup.feature
+++ features/schedule_a_meetup.feature
@@ -11,3 +11,11 @@ Feature:
     And I am on "/"
     Then I should see "Upcoming meetups"
     And I should see "Coding Dojo"
+
+  Scenario: RSVP to a meetup
+    Given I am logged in as "Regular user"
+    And a meetup was scheduled
+    And I am on the detail page of this meetup
+    When I press "RSVP"
+    Then I should see "You have successfully RSVP-ed to this meetup"
+    And the list of attendees should contain "Regular user"
