5a12b26304d2aea12332c1871717e005bd21b076
Matthias Noback <matthiasnoback@gmail.com>
2019-10-10 14:13:38 +0200

    Assignment 6: Introduce a read model for upcoming and past meetups

--- src/MeetupOrganizing/ServiceContainer.php
+++ src/MeetupOrganizing/ServiceContainer.php
@@ -15,6 +15,7 @@ use MeetupOrganizing\Controller\SwitchUserController;
 use MeetupOrganizing\Entity\MeetupRepository;
 use MeetupOrganizing\Entity\RsvpRepository;
 use MeetupOrganizing\Entity\UserRepository;
+use MeetupOrganizing\ReadModel\ListMeetupsRepository;
 use MeetupOrganizing\Resources\Views\FlashExtension;
 use MeetupOrganizing\Resources\Views\TwigTemplates;
 use MeetupOrganizing\Resources\Views\UserExtension;
@@ -183,6 +184,11 @@ final class ServiceContainer extends Container
                 $this[Connection::class]
             );
         };
+        $this[ListMeetupsRepository::class] = function () {
+            return new ListMeetupsRepository(
+                $this[Connection::class]
+            );
+        };
 
         /*
          * Controllers
@@ -210,7 +216,7 @@ final class ServiceContainer extends Container
         };
         $this[ListMeetupsController::class] = function () {
             return new ListMeetupsController(
-                $this[Connection::class],
+                $this[ListMeetupsRepository::class],
                 $this[TemplateRendererInterface::class]
             );
         };
