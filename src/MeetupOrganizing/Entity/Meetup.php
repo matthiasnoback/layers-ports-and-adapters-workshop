cb0093cf11603ee999e79e5b63d1db768cd5888c
Matthias Noback <matthiasnoback@gmail.com>
2019-10-17 10:29:59 +0200

    Assignment 1: Introduce Meetup entity

---
+++ src/MeetupOrganizing/Entity/Meetup.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace MeetupOrganizing\Entity;
+
+final class Meetup
+{
+    /**
+     * @var UserId
+     */
+    private $organizerId;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var ScheduledDate
+     */
+    private $scheduledFor;
+
+    /**
+     * @var bool
+     */
+    private $wasCancelled = false;
+
+    public function __construct(
+        UserId $organizerId,
+        string $name,
+        string $description,
+        ScheduledDate $scheduledFor
+    ) {
+        $this->organizerId = $organizerId;
+        $this->name = $name;
+        $this->description = $description;
+        $this->scheduledFor = $scheduledFor;
+    }
+
+    public function getData(): array
+    {
+        return [
+            'organizerId' => $this->organizerId->asInt(),
+            'name' => $this->name,
+            'description' => $this->description,
+            'scheduledFor' => $this->scheduledFor->asString(),
+            'wasCancelled' => (int)$this->wasCancelled
+        ];
+    }
+}
