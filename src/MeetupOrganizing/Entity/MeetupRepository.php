7142df9447cf781f41bf883f7c13f0b8edef304e
Matthias Noback <matthiasnoback@gmail.com>
2019-10-17 14:39:51 +0200

    Assignment 5: Introducing a view model and repository

--- src/MeetupOrganizing/Entity/MeetupRepository.php
+++ src/MeetupOrganizing/Entity/MeetupRepository.php
@@ -3,9 +3,15 @@ declare(strict_types=1);
 
 namespace MeetupOrganizing\Entity;
 
+use Assert\Assert;
+use DateTimeImmutable;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\Statement;
+use MeetupOrganizing\ReadModel\ListMeetupsRepository;
+use MeetupOrganizing\ReadModel\MeetupForList;
+use PDO;
 
-final class MeetupRepository
+final class MeetupRepository implements ListMeetupsRepository
 {
     private Connection $connection;
 
@@ -23,5 +29,39 @@ final class MeetupRepository
 
         $meetupId = (int)$this->connection->lastInsertId();
         $meetup->setId($meetupId);
+    }
+
+    public function upcomingMeetups(DateTimeImmutable $now): array
+    {
+        $statement = $this->connection->createQueryBuilder()
+            ->select('*')
+            ->from('meetups')
+            ->where('scheduledFor >= :now')
+            ->setParameter('now', $now->format(ScheduledDate::DATE_TIME_FORMAT))
+            ->andWhere('wasCancelled = :wasNotCancelled')
+            ->setParameter('wasNotCancelled', 0)
+            ->execute();
+        Assert::that($statement)->isInstanceOf(Statement::class);
+
+        $upcomingMeetups = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return array_map([MeetupForList::class, 'fromDatabaseRecord'], $upcomingMeetups);
+    }
+
+    public function pastMeetups(DateTimeImmutable $now): array
+    {
+        $statement = $this->connection->createQueryBuilder()
+            ->select('*')
+            ->from('meetups')
+            ->where('scheduledFor < :now')
+            ->setParameter('now', $now->format(ScheduledDate::DATE_TIME_FORMAT))
+            ->andWhere('wasCancelled = :wasNotCancelled')
+            ->setParameter('wasNotCancelled', 0)
+            ->execute();
+        Assert::that($statement)->isInstanceOf(Statement::class);
+
+        $pastMeetups = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return array_map([MeetupForList::class, 'fromDatabaseRecord'], $pastMeetups);
     }
 }
