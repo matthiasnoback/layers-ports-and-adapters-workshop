e38ab6a29938b8e64ffd208f4bb8b95b9ce3da88
Matthias Noback <matthiasnoback@gmail.com>
2019-10-17 15:32:07 +0200

    Assignment 7: Layers

---
+++ src/MeetupOrganizing/Infrastructure/Session.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace MeetupOrganizing\Infrastructure;
+
+use MeetupOrganizing\Domain\User;
+use MeetupOrganizing\Domain\UserId;
+use MeetupOrganizing\Infrastructure\UserRepository;
+
+final class Session
+{
+    private const DEFAULT_userId = 1;
+    private const LOGGED_IN_userId = 'logged_in_userId';
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var array
+     */
+    private $sessionData;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        if (php_sapi_name() === 'cli') {
+            $this->sessionData = [];
+        } else {
+            session_start();
+            $this->sessionData = &$_SESSION;
+        }
+
+        $this->userRepository = $userRepository;
+    }
+
+    public function getLoggedInUser(): User
+    {
+        return $this->userRepository->getById(
+            UserId::fromInt(
+                (int)$this->get(self::LOGGED_IN_userId, self::DEFAULT_userId)
+            )
+        );
+    }
+
+    public function setLoggedInUserId(UserId $id): void
+    {
+        $this->set(self::LOGGED_IN_userId, $id->asInt());
+    }
+
+    public function get(string $key, $defaultValue = null)
+    {
+        if (isset($this->sessionData[$key])) {
+            return $this->sessionData[$key];
+        }
+
+        return $defaultValue;
+    }
+
+    public function set(string $key, $value): void
+    {
+        $this->sessionData[$key] = $value;
+    }
+
+    public function addErrorFlash(string $message): void
+    {
+        $this->addFlash('danger', $message);
+    }
+
+    public function addSuccessFlash(string $message): void
+    {
+        $this->addFlash('success', $message);
+    }
+
+    private function addFlash(string $type, string $message): void
+    {
+        $this->sessionData['flashes'][$type][] = $message;
+    }
+
+    public function getFlashes(): array
+    {
+        $flashes = $this->sessionData['flashes'] ?? [];
+
+        $this->sessionData['flashes'] = [];
+
+        return $flashes;
+    }
+}
