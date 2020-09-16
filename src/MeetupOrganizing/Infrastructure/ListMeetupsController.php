9c8a533a1daaa4893337d97108a6289e183da6c3
Matthias Noback <matthiasnoback@gmail.com>
2020-09-16 16:21:39 +0200

    Assignment 6: Introducing directories for each layer

---
+++ src/MeetupOrganizing/Infrastructure/ListMeetupsController.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace MeetupOrganizing\Infrastructure;
+
+use DateTimeImmutable;
+use MeetupOrganizing\Application\ListMeetupsRepository;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Stratigility\MiddlewareInterface;
+
+final class ListMeetupsController implements MiddlewareInterface
+{
+    private ListMeetupsRepository $listMeetupsRepository;
+
+    private TemplateRendererInterface $renderer;
+
+    public function __construct(
+        ListMeetupsRepository $listMeetupsRepository,
+        TemplateRendererInterface $renderer
+    ) {
+        $this->listMeetupsRepository = $listMeetupsRepository;
+        $this->renderer = $renderer;
+    }
+
+    public function __invoke(Request $request, Response $response, callable $out = null): ResponseInterface
+    {
+        $now = new DateTimeImmutable();
+
+        $response->getBody()->write(
+            $this->renderer->render(
+                'list-meetups.html.twig',
+                [
+                    'upcomingMeetups' => $this->listMeetupsRepository->upcomingMeetups($now),
+                    'pastMeetups' => $this->listMeetupsRepository->pastMeetups($now)
+                ]));
+
+        return $response;
+    }
+}
