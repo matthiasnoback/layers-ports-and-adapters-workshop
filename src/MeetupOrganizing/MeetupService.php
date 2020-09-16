1a2e5dfb67a545ce4d7efe63423a6c3989074c50
Matthias Noback <matthiasnoback@gmail.com>
2020-09-16 12:17:42 +0200

    Assignment 2: Introducing an application service

---
+++ src/MeetupOrganizing/MeetupService.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace MeetupOrganizing;
+
+use Assert\Assert;
+use MeetupOrganizing\Entity\Meetup;
+use MeetupOrganizing\Entity\MeetupRepository;
+use MeetupOrganizing\Entity\ScheduledDate;
+use MeetupOrganizing\Entity\UserId;
+use MeetupOrganizing\Entity\UserRepository;
+
+final class MeetupService
+{
+    private UserRepository $userRepository;
+
+    private MeetupRepository $meetupRepository;
+
+    public function __construct(
+        UserRepository $userRepository,
+        MeetupRepository $meetupRepository
+    ) {
+        $this->userRepository = $userRepository;
+        $this->meetupRepository = $meetupRepository;
+    }
+
+    public function scheduleMeetup(
+        int $organizerId,
+        string $name,
+        string $description,
+        string $scheduledFor
+    ): int {
+        $user = $this->userRepository->getById(UserId::fromInt($organizerId));
+
+        $meetup = Meetup::schedule(
+            $user->userId(),
+            $name,
+            $description,
+            ScheduledDate::fromString(
+                $scheduledFor
+            )
+        );
+
+        $this->meetupRepository->save($meetup);
+
+        Assert::that($meetup->getId())->integer();
+
+        return $meetup->getId();
+    }
+}
