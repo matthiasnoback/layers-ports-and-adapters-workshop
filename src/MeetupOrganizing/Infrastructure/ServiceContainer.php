f907873332cc9ae28fe24bb6f8f2ddc71c1d203f
Matthias Noback <matthiasnoback@gmail.com>
2020-10-28 16:22:33 +0100

    Assignment 7: Applying the DIP

--- src/MeetupOrganizing/Infrastructure/ServiceContainer.php
+++ src/MeetupOrganizing/Infrastructure/ServiceContainer.php
@@ -8,7 +8,8 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use MeetupOrganizing\Application\ListMeetupsRepository;
 use MeetupOrganizing\Application\MeetupService;
-use MeetupOrganizing\Infrastructure\MeetupRepository;
+use MeetupOrganizing\Domain\MeetupRepository;
+use MeetupOrganizing\Infrastructure\MeetupRepositoryUsingSql;
 use MeetupOrganizing\Domain\RsvpRepository;
 use MeetupOrganizing\Domain\UserRepository;
 use MeetupOrganizing\Infrastructure\Resources\Views\FlashExtension;
@@ -175,11 +176,14 @@ final class ServiceContainer extends Container
             );
         };
         $this[MeetupRepository::class] = function () {
-            return new MeetupRepository(
+            return new MeetupRepositoryUsingSql(
                 $this[Connection::class]
             );
         };
-        $this[ListMeetupsRepository::class] = $this[MeetupRepository::class];
+
+        $this[ListMeetupsRepository::class] = function () {
+            return $this[MeetupRepository::class];
+        };
 
         /*
          * Controllers
